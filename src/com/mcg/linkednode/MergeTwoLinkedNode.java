7566e246f8ae50693309160fdf1d3c73a6ca59ef
maochungaung <mcg915881127@163.com>
2018-08-01 00:17:14 +0800

    合并有序链表

--- src/com/mcg/linkednode/MergeTwoLinkedNode.java
+++ src/com/mcg/linkednode/MergeTwoLinkedNode.java
@@ -21,9 +21,29 @@ public class MergeTwoLinkedNode {
      * }
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // TODO
-
-        return null;
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+        }
+        temp.next = l1 != null ? l1 : l2;
+        return head.next;
     }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
 
 }
