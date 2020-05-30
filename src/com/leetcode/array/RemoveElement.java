ff726bb898edc6a05f7b9064fa888b96c32eb909
maochunguang <maochunguang@meituan.com>
2020-05-30 15:56:31 +0800

    包名修改

--- src/com/leetcode/array/RemoveElement.java
+++ src/com/leetcode/array/RemoveElement.java
@@ -13,6 +13,14 @@ package com.leetcode.array;
  */
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        return 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != val){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
     }
+
 }
