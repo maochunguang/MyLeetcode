6964d2f08518eb2dc83d306dfdf956e4e05b9254
maochunguang <mcg915881127@163.com>
2020-06-28 23:22:36 +0800

    最长括号

--- src/main/java/com/leetcode/string/ValidParentheses.java
+++ src/main/java/com/leetcode/string/ValidParentheses.java
@@ -3,14 +3,28 @@ package com.leetcode.string;
 /**
  * @author maocg
  * Date：2018/4/7
- * Description：最长有效括号
+ * Description：【32】最长有效括号
  * 给一个只包含 '(' 和 ')' 的字符串，找出最长的有效（正确关闭）括号子串的长度。
- * 对于 "(()"，最长有效括号子串为 "()" ，它的长度是 2。
- * 另一个例子 ")()())"，最长有效括号子串为 "()()"，它的长度是 4。
+ * <p>
+ * 示例 1:
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ * <p>
+ * 示例 2:
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ * Related Topics 字符串 动态规划
  */
 public class ValidParentheses {
+    /**
+     * 第一种方案
+     * @param s 输入字符串
+     * @return 返回长度
+     */
     public int longestValidParentheses(String s) {
-        // TODO
+
         return 0;
     }
 
