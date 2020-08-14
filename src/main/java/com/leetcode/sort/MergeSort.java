97bbdc92936e2388b124e2f0839c66694e35a663
maochunguang <mcg915881127@163.com>
2020-08-14 17:03:06 +0800

    两路归并

--- src/main/java/com/leetcode/sort/MergeSort.java
+++ src/main/java/com/leetcode/sort/MergeSort.java
@@ -17,29 +17,36 @@ public class MergeSort {
         }
     }
 
+    //两路归并
     private static void merge(int[] a, int low, int mid, int high) {
         // 复制到aux[]
-        int[] aux = new int[a.length];
-        for (int k = low; k <= high; k++) {
-            aux[k] = a[k];
-        }
+        int[] temp = new int[a.length];
+        int p1 = low, p2 = mid + 1, k = low;//p1、p2是检测指针，k是存放指针
         // 合并回 a[],i代表左边数组下标，j带有右边数组下标
-        int i = low, j = mid + 1;
-        for (int k = low; k <= high; k++) {
-            if (i > mid) {
-                a[k] = aux[j++];
-            } else if (j > high) {
-                a[k] = aux[i++];
-            } else if (aux[j] < aux[i]) {
-                a[k] = aux[j++];
+        while (p1 <= mid && p2 <= high) {
+            if (a[p1] <= a[p2]) {
+                temp[k++] = a[p1++];
             } else {
-                a[k] = aux[i++];
+                temp[k++] = a[p2++];
             }
+        }
+        while (p1 <= mid) {
+            temp[k++] = a[p1++];
+        }
+        while (p2 <= high) {
+            temp[k++] = a[p2++];
+        }
+        //copy到原数组
+        for (int i = low; i <= high; i++) {
+            a[i] = temp[i];
         }
     }
 
+
     public static void main(String[] args) {
         int[] arr = {49, 38, 65, 97, 76, 13, 27, 4, 78, 34, 12, 64, 1, 8};
+        System.out.println("排序之前：");
+        System.out.println(Arrays.toString(arr));
         sort(arr, 0, arr.length - 1);
         System.out.println("排序之后：");
         System.out.println(Arrays.toString(arr));
