0e4907cd8fb8efc15038f3474d4139b18cef97c2
maochunguang <mcg915881127@163.com>
2020-08-18 21:27:31 +0800

    外观数列

---
+++ src/main/java/com/leetcode/inacion/LRUCache.java
@@ -0,0 +1,9 @@
+package com.leetcode.inacion;
+
+/**
+ * @author mao
+ * Date：2020/8/18
+ * Description：
+ */
+public class LRUCache {
+}
---
+++ src/main/java/com/leetcode/string/DifferentStr.java
@@ -0,0 +1,10 @@
+package com.leetcode.string;
+
+/**
+ * @author mao
+ * Date：2020/8/18
+ * Description：给定两个字符串，只有一个字符不同，找出这个字符。
+ */
+public class DifferentStr {
+
+}
