bb83f6f6917677999ce38ba7920e5183e07b98fe
maochunguang <maochunguang@meituan.com>
2020-08-18 14:29:38 +0800

    二叉树

--- src/main/java/com/leetcode/tree/BinaryTree.java
+++ src/main/java/com/leetcode/tree/BinaryTree.java
@@ -3,7 +3,7 @@ package com.leetcode.tree;
 /**
  * @author mao
  * Date：2020/8/17
- * Description：
+ * Description：二叉树基本操作，前序，中序，后序遍历
  */
 public class BinaryTree {
 }
--- src/main/java/com/leetcode/tree/ZPrintTree.java
+++ src/main/java/com/leetcode/tree/ZPrintTree.java
@@ -74,12 +74,3 @@ public class ZPrintTree {
     }
 }
 
-class TreeNode {
-    TreeNode left;
-    TreeNode right;
-    int value;
-
-    TreeNode(int value) {
-        this.value = value;
-    }
-}
