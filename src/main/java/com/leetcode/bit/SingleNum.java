0c543752ef57e304b889b357ff53b09f68fde765
maochunguang <maochunguang@meituan.com>
2020-09-15 15:55:49 +0800

    位运算

--- src/main/java/com/leetcode/bit/SingleNum.java
+++ src/main/java/com/leetcode/bit/SingleNum.java
@@ -16,6 +16,15 @@ package com.leetcode.bit;
  * 输出: 4
  */
 public class SingleNum {
+
+    public static void main(String[] args) {
+        SingleNum singleNum = new SingleNum();
+        int[] nums = new int[]{1, 1, 2, 1};
+        int[] nums1 = new int[]{1, 1, 2, 1, 2, 3, 2};
+        System.out.println(singleNum.singleNumber2(nums));
+        System.out.println(singleNum.singleNumber2(nums1));
+    }
+
     /**
      * 使用异或运算
      * @param nums
@@ -28,4 +37,28 @@ public class SingleNum {
         }
         return sum;
     }
+
+    /**
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。
+     * 说明：你的算法应该具有线性时间复杂度。你可以不使用额外空间来实现吗？
+     * 示例 1:
+     * 输入: [2,2,3,2]
+     * 输出: 3
+     *
+     * 示例 2:
+     * 输入: [0,1,0,1,0,1,99]
+     * 输出: 99
+     * @param nums
+     * @return
+     */
+    public int singleNumber2(int[] nums) {
+        int a = 0, b = 0;
+        for (int next : nums) {
+            b = (b ^ next) & ~a;
+            a = (a ^ next) & ~b;
+            System.out.println("a==" + a + "; b==" + b);
+        }
+        return b;
+    }
+
 }
