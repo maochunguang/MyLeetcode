e0297e9d51c0f22253021f94dbb06def7cfdab3d
maochunguang <mcg915881127@163.com>
2020-09-15 23:27:22 +0800

    合并k个链表

--- src/main/java/com/leetcode/linkednode/MergeLinkedNode.java
+++ src/main/java/com/leetcode/linkednode/MergeLinkedNode.java
@@ -1,5 +1,8 @@
 package com.leetcode.linkednode;
 
+import com.leetcode.utils.FakerData;
+import com.leetcode.utils.PrintHelper;
+
 /**
  * @author maocg
  * Date：2018/4/6
@@ -10,7 +13,17 @@ package com.leetcode.linkednode;
  * 输入：1->2->4, 1->3->4
  * 输出：1->1->2->3->4->4
  */
-public class MergeTwoLinkedNode {
+public class MergeLinkedNode {
+    public static void main(String[] args) {
+        MergeLinkedNode mergeLinkedNode = new MergeLinkedNode();
+        ListNode l1 = FakerData.initNodeFromArr(new int[]{1, 3, 5, 7});
+        ListNode l2 = FakerData.initNodeFromArr(new int[]{2, 6, 8});
+        ListNode l3 = FakerData.initNodeFromArr(new int[]{4, 9, 11});
+        ListNode[] listNodes = new ListNode[]{l1, l2, l3};
+        PrintHelper.printNode(mergeLinkedNode.mergeKLists(listNodes));
+        PrintHelper.printNode(mergeLinkedNode.mergeTwoListsRe(l1, l2));
+        PrintHelper.printNode(mergeLinkedNode.mergeTwoListsRe(l2, l3));
+    }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
@@ -59,5 +72,26 @@ public class MergeTwoLinkedNode {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
+    }
+
+    /**
+     * 合并k个链表
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        ListNode pre = new ListNode(-1);
+        ListNode preHeader = pre;
+        ListNode cur = lists[0];
+        pre.next = cur;
+        for (int i = 1; i < lists.length; i++) {
+            pre.next = mergeTwoLists(cur, lists[i]);
+            cur = pre.next;
+        }
+
+        return preHeader.next;
     }
 }
